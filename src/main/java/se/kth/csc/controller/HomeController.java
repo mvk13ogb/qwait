f22ed96e432205918927edf615f5a92a81ce98f0
David Flemström <david.flemstrom@gmail.com>
2013-12-24 17:56:04 +0100

    Add debug endpoints for making an user into an admin

--- src/main/java/se/kth/csc/controller/HomeController.java
+++ src/main/java/se/kth/csc/controller/HomeController.java
@@ -1,14 +1,32 @@
 package se.kth.csc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import se.kth.csc.model.Account;
+import se.kth.csc.persist.AccountStore;
+
+import java.security.Principal;
 
 /**
  * Controls the home page.
  */
 @Controller
 public class HomeController {
+    private final AccountStore accountStore;
+
+    protected HomeController() {
+        // Needed for injection
+        accountStore = null;
+    }
+
+    @Autowired
+    public HomeController(AccountStore accountStore) {
+        this.accountStore = accountStore;
+    }
 
     /**
      * The index page of the web application.
@@ -16,6 +34,32 @@ public class HomeController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
         return "home";
+    }
+
+    @Transactional
+    @RequestMapping(value = "/make-me-admin", method = RequestMethod.POST)
+    public String makeMeAdmin(Principal principal) {
+        // Make user into an admin
+        Account account = accountStore.fetchAccountWithPrincipalName(principal.getName());
+        account.setAdmin(true);
+
+        // Log out user to reload auth roles
+        SecurityContextHolder.clearContext();
+
+        return "redirect:/";
+    }
+
+    @Transactional
+    @RequestMapping(value = "/make-me-not-admin", method = RequestMethod.POST)
+    public String makeMeNotAdmin(Principal principal) {
+        // Make user into a non-admin
+        Account account = accountStore.fetchAccountWithPrincipalName(principal.getName());
+        account.setAdmin(false);
+
+        // Log out user to reload auth roles
+        SecurityContextHolder.clearContext();
+
+        return "redirect:/";
     }
 }
 
