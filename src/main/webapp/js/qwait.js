8521f04861744bde06e6243af12deb44da2c79a8
Hampus Liljekvist <hlilje@server.fake>
2014-01-20 11:21:23 +0100

    add time in queue to queue list

--- src/main/webapp/js/qwait.js
+++ src/main/webapp/js/qwait.js
@@ -10,3 +10,17 @@ qwait.filter('timestamp', function() {
         return new Date(milliseconds);
     }
 });
+
+qwait.filter('queuetime', function() {
+    return function (milliseconds) {
+        p = Math.floor((new Date() - milliseconds)/1000);
+        x = p;
+        s = x % 60;
+        x = Math.floor(x/60)
+        m = x % 60;
+        x = Math.floor(x/60)
+        h = x % 24;
+
+        return p < 86400 ? h + 'h ' + m + 'm ' + s + 's' : 'Over a day';
+    }
+});
