ca9e6e1220e303c5662e665febca7ca03beb7941
Hampus Liljekvist <hlilje@server.fake>
2014-01-26 13:23:37 +0100

    Declare js variables and remove seconds

--- src/main/webapp/js/qwait.js
+++ src/main/webapp/js/qwait.js
@@ -13,14 +13,12 @@ qwait.filter('timestamp', function() {
 
 qwait.filter('queuetime', function() {
     return function (milliseconds) {
-        p = Math.floor((new Date() - milliseconds)/1000);
-        x = p;
-        s = x % 60;
-        x = Math.floor(x/60)
-        m = x % 60;
-        x = Math.floor(x/60)
-        h = x % 24;
+        var p = Math.floor((new Date() - milliseconds)/1000);
+        var x = Math.floor(p/60);
+        var m = x % 60;
+        x = Math.floor(x/60);
+        var h = x % 24;
 
-        return p < 86400 ? h + 'h ' + m + 'm ' + s + 's' : 'Over a day';
+        return p < 86400 ? h + 'h ' + m + 'm' : 'Over a day';
     }
 });
