0f3dea99ab71838bfce44c11976fb60cad9ad8ba
Casper Winsnes <cwinsnes92@gmail.com>
2014-02-09 14:36:29 +0100

    Make it possible for superadmin to add and remove admins

--- src/main/java/se/kth/csc/controller/SuperadminController.java
+++ src/main/java/se/kth/csc/controller/SuperadminController.java
@@ -6,10 +6,12 @@ import com.google.common.collect.ImmutableMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import se.kth.csc.model.Account;
 import se.kth.csc.persist.AccountStore;
@@ -30,20 +32,44 @@ public class SuperadminController {
     public SuperadminController(
             AccountStore accountStore
     ) {
-       this.accountStore = accountStore;
+        this.accountStore = accountStore;
     }
 
     @RequestMapping(value = "/superadmin", method = RequestMethod.GET)
     public ModelAndView superadminsettings() {
         return new ModelAndView();
     }
-   private Account getCurrentAccount(Principal principal) {
-       return accountStore.fetchAccountWithPrincipalName(principal.getName());
-   }
+    private Account getCurrentAccount(Principal principal) {
+        return accountStore.fetchAccountWithPrincipalName(principal.getName());
+    }
 
-   @Transactional
-   @RequestMapping(value="/makeadmin", method= RequestMethod.POST)
-   public void makeUserAdmin(Account acc) {
-       acc.setAdmin(true);
-   }
+    @Transactional
+    @RequestMapping(value="/makeadmin", method = RequestMethod.POST)
+    public String makeUserAdmin(@RequestParam("name") String adminName) {
+        Account account = accountStore.fetchAccountWithPrincipalName(adminName);
+        if(account == null) {
+            log.info("Account " + adminName + " could not be found");
+            return "redirect:/superadmin/superadmin";
+        }
+        account.setAdmin(true);
+        log.info(adminName + " made admin");
+
+        SecurityContextHolder.clearContext();
+        return "redirect:/superadmin/superadmin";
+    }
+
+    @Transactional
+    @RequestMapping(value="/removeadmin", method = RequestMethod.POST)
+    public String removeUserAdmin(@RequestParam("name") String adminName) {
+        Account account = accountStore.fetchAccountWithPrincipalName(adminName);
+        if(account == null) {
+            log.info("Account " + adminName + " could not be found");
+            return "redirect:/superadmin/superadmin";
+        }
+        account.setAdmin(false);
+        log.info(adminName + " removed from admin");
+
+        SecurityContextHolder.clearContext();
+        return "redirect:/superadmin/superadmin";
+    }
 }
