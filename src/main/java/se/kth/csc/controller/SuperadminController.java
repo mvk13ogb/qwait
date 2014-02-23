fa7f093430a02bddcf6581800733517e78e003c8
Casper Winsnes <cwinsnes92@gmail.com>
2014-02-23 12:50:30 +0100

    Add code for adding and removing queue owners

--- src/main/java/se/kth/csc/controller/SuperadminController.java
+++ src/main/java/se/kth/csc/controller/SuperadminController.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +16,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import se.kth.csc.model.Account;
+import se.kth.csc.model.Queue;
 import se.kth.csc.persist.AccountStore;
+import se.kth.csc.persist.QueueStore;
 
 import java.security.Principal;
 
@@ -24,28 +27,41 @@ import java.security.Principal;
 public class SuperadminController {
     private static final Logger log = LoggerFactory.getLogger(SuperadminController.class);
     private final AccountStore accountStore;
+    private final QueueStore queueStore;
     private final ObjectMapper objectMapper;
 
     protected SuperadminController() {
         accountStore = null;
         objectMapper = null;
+        queueStore = null;
     }
 
     @Autowired
     public SuperadminController(
             AccountStore accountStore,
-            ObjectMapper objectMapper
+            ObjectMapper objectMapper,
+            QueueStore queueStore
     ) {
         this.accountStore = accountStore;
         this.objectMapper = objectMapper;
+        this.queueStore = queueStore;
     }
 
     @RequestMapping(value = "/superadmin", method = RequestMethod.GET)
     public ModelAndView superadminsettings(Principal principal) throws JsonProcessingException {
         Account account = getCurrentAccount(principal);
+
+        //A superadmin should be able to add and remove people on EVERY queue
+        //but should not keep owner priviliges for every queue if demoted from superadmin.
+        //Might be changed into making every superadmin an actual queueowner.
+        if(account.isSuperAdmin()) {
+            account = new Account();
+            account.setQueues(Sets.newHashSet(queueStore.fetchAllQueues()));
+        }
         String accountJson = objectMapper.writerWithView(Account.class).writeValueAsString(account);
         return new ModelAndView("superadmin/superadmin", ImmutableMap.of("account", account, "accountJson", accountJson));
     }
+
     private Account getCurrentAccount(Principal principal) {
         return accountStore.fetchAccountWithPrincipalName(principal.getName());
     }
@@ -67,7 +83,38 @@ public class SuperadminController {
 
     @Transactional
     @RequestMapping(value = "/addqueueowner", method = RequestMethod.POST)
-    public String addQueueOwner() {
+    public String addQueueOwner(@RequestParam("name") String newQueueOwner,
+                                @RequestParam("queueName") String queueId) {
+        Account account = accountStore.fetchAccountWithPrincipalName(newQueueOwner);
+        if(account == null) {
+            log.info("Account " + newQueueOwner + " could not be found");
+            return "redirect:/superadmin/superadmin";
+        }
+        int id = Integer.parseInt(queueId);
+        Queue queue = queueStore.fetchQueueWithId(id);
+        queue.addOwner(account);
+        log.info("Queue with id " + id + " now has " + newQueueOwner
+                + " as a queue owner");
+        return "redirect:/superadmin/superadmin";
+    }
+
+    @Transactional
+    @RequestMapping(value = "/removequeueowner", method = RequestMethod.POST)
+    public String removeQueueOwner(@RequestParam("name") String oldOwnerName,
+                                   @RequestParam("queueName") String queueId) {
+        Account account = accountStore.fetchAccountWithPrincipalName(oldOwnerName);
+        if(account == null) {
+            log.info("Account " + oldOwnerName + " could not be found");
+            return "redirect:/superadmin/superadmin";
+        }
+        int id = Integer.parseInt(queueId);
+        Queue queue = queueStore.fetchQueueWithId(id);
+        if(queue.getOwners().size() == 1) {
+            log.info("Cannot remove last owner of queue with id " + id);
+            return "redirect:/superadmin/superadmin";
+        }
+        queue.removeOwner(account);
+        log.info(oldOwnerName + " remove from ownerlist of queue with id " + id);
         return "redirect:/superadmin/superadmin";
     }
 
