a5e71db23f4d555ef1cb62290418f8ae950034b6
Schmisk <schmidt.0250@gmail.com>
2014-03-03 13:58:55 +0100

    Add file for testing QwaitFuncs

---
+++ src/test/webapp/js/testingQwaitFuncs.spec.js
@@ -0,0 +1,7 @@
+include('~/Documents/GitDir/qwait/src/main/webapp/js/qwait.js');
+
+describe('qwait.filter functions', function() {
+    it('contains a timestamp function that returns the date from milliseconds', function() {
+        
+    });
+});
