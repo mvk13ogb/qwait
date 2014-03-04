be53fca6d279eaa89b3f2d631fc2156da185d5bb
David Flemström <dflemstr@spotify.com>
2014-03-04 07:46:41 -0500

    Add example test to show how it's done
    I added a lot more comments than necessary, to explain what is going on.
    
    Change-Id: I20ab65e93fb69f54185b6c479119f01977b202be

---
+++ src/test/java/se/kth/csc/controller/HomeControllerTest.java
@@ -0,0 +1,53 @@
+package se.kth.csc.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import se.kth.csc.model.Account;
+import se.kth.csc.persist.AccountStore;
+
+import java.security.Principal;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class HomeControllerTest {
+
+  // System under test (SUT):
+  private HomeController homeController;
+
+  // Dependencies of this unit
+  private AccountStore accountStore;
+
+  @Before
+  public void setUp() throws Exception {
+    // Mock dependencies
+    accountStore = mock(AccountStore.class);
+
+    // Create SUT
+    homeController = new HomeController(accountStore);
+  }
+
+  @Test
+  public void testMakeMeAdmin() {
+    // Set up expected behavior of mocks
+    Principal principal = mock(Principal.class);
+    when(principal.getName()).thenReturn("testuser");
+
+    Account account = mock(Account.class);
+    when(accountStore.fetchAccountWithPrincipalName("testuser")).thenReturn(account);
+
+    // Run the system under test
+    String result = homeController.makeMeAdmin(principal);
+
+    // Verify that interactions with mocks happened
+    verify(account, atLeastOnce()).setAdmin(true);
+
+    // Verify actual returned results
+    assertEquals("redirect:/debug", result);
+  }
+}
