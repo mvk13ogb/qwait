91833be21af692340f95cb32c763ef0bf3f4de6c
Casper Winsnes <cwinsnes92@gmail.com>
2014-03-04 18:31:23 +0100

    Create AdminControllerTest

---
+++ src/test/java/se/kth/csc/controller/AdminControllerTest.java
@@ -0,0 +1,10 @@
+package se.kth.csc.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import se.kth.csc.persist.AccountStore;
+
+public class AdminControllerTest {
+
+    private AccountStore accountStore;
+    private ObjectMapper objectMapper;
+}
