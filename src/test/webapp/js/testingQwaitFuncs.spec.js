41ffea33d3469210f6f5da3ca3a63886bb2ba6bf
David Flemström <dflemstr@spotify.com>
2014-03-09 12:37:26 -0400

    Remove old tests
    Change-Id: I22a02589fd71d78da7eab49bd99e7de0fb4271fa

--- src/test/webapp/js/testingQwaitFuncs.spec.js
+++
@@ -1,7 +0,0 @@
-include('~/Documents/GitDir/qwait/src/main/webapp/js/qwait.js');
-
-describe('qwait.filter functions', function() {
-    it('contains a timestamp function that returns the date from milliseconds', function() {
-        
-    });
-});
