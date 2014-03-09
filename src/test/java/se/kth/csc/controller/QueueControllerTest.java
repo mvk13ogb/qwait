cc8ca8557b963855108454cc9ba6c4fb5079a49a
Hampus Liljekvist <hlilje@users.noreply.github.com>
2014-03-09 16:37:35 +0100

    Add create queue test

--- src/test/java/se/kth/csc/controller/QueueControllerTest.java
+++ src/test/java/se/kth/csc/controller/QueueControllerTest.java
@@ -1,29 +1,22 @@
 package se.kth.csc.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.collect.ImmutableMap;
 import org.junit.Before;
 import org.junit.Test;
 
-import org.mockito.ArgumentCaptor;
-import org.springframework.web.servlet.ModelAndView;
 import se.kth.csc.auth.Role;
 import se.kth.csc.model.Account;
 import se.kth.csc.model.Queue;
+import se.kth.csc.model.QueuePosition;
 import se.kth.csc.payload.QueueCreationInfo;
 import se.kth.csc.persist.AccountStore;
-import se.kth.csc.persist.JPAStore;
 import se.kth.csc.persist.QueuePositionStore;
 import se.kth.csc.persist.QueueStore;
 
 import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
-import java.util.LinkedList;
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 
 public class QueueControllerTest {
@@ -65,7 +58,6 @@ public class QueueControllerTest {
 
         String queuesJson = null;
         ModelAndView result = null;
-        ModelAndView expected = mock(ModelAndView.class);
         try {
             queuesJson = objectMapper.writerWithView(Queue.class).writeValueAsString(queues);
             result = queueController.list(request);
@@ -73,19 +65,22 @@ public class QueueControllerTest {
             // Do nothing
         }
 
-        ImmutableMap<String, Object> modelViewMap = ImmutableMap.of("queues", queues, "queuesJson", queuesJson);
-        when(expected.getModel()).thenReturn(modelViewMap);
-
         assertEquals(expected, result);
     }*/
 
-    /*@Test
+    /**
+     * Only tests if _a_ queue is stored due to object creation in method.
+     */
+    @Test
     public void testCreate() {
         Principal principal = mock(Principal.class);
         when(principal.getName()).thenReturn("testuser");
 
         Account owner = mock(Account.class);
         when(accountStore.fetchAccountWithPrincipalName("testuser")).thenReturn(owner);
+
+        Queue queue = mock(Queue.class);
+        when(queueStore.fetchQueueWithId(anyInt())).thenReturn(queue); // Ignore the id value
 
         HttpServletRequest request = mock(HttpServletRequest.class);
         when(request.getUserPrincipal()).thenReturn(principal);
@@ -94,26 +89,16 @@ public class QueueControllerTest {
         QueueCreationInfo queueCreationInfo = mock(QueueCreationInfo.class);
         when(queueCreationInfo.getName()).thenReturn("testqueue");
 
-//        queueStore = spy(queueStore); // Should call the methods on the real object
-
         String result = "";
         try {
             result = queueController.create(queueCreationInfo, request, principal);
         } catch (Exception e) {
             // Do nothing
         }
-        //verify(queuePositionStore, atLeastOnce()).storeQueuePosition(null);
-
-        List<Queue> queues = queueStore.fetchAllActiveQueues();
-        Queue queue = queues.get(0);
-        assertEquals(queues.size(), 1);
-        assertEquals(queue.getName(), "testqueue");
-        assertTrue(queue.isActive());
-        assertFalse(queue.isLocked());
-        assertTrue(queue.getOwners().contains(owner));
+        verify(queueStore, atLeastOnce()).storeQueue(any(Queue.class));
 
         assertEquals("redirect:/queue/list", result);
-    }*/
+    }
 
     @Test
     public void testOpen() {
