2baabfe7c817c4cf495f9e215c6e896210da7259
michaelhakansson <michael.hakansson@gmail.com>
2014-04-02 15:39:54 +0200

    Add gravatar and Twitter for Michael

--- src/main/webapp/js/qwait.js
+++ src/main/webapp/js/qwait.js
@@ -210,10 +210,10 @@
                 { name: 'Christoffer Pettersson' },
                 { name: 'David Flemström', gravatar: '202ecb437d8bbd442d093a3a35c67a04', twitter: 'dflemstr' },
                 { name: 'Eric Schmidt' },
-                { name: 'Gustav Zander', gravatar: '354a77646cf4a560ea5d5357a5a4aa84'},
+                { name: 'Gustav Zander', gravatar: '354a77646cf4a560ea5d5357a5a4aa84' },
                 { name: 'Jacob Sievers' },
-                { name: 'Michael Håkansson' },
-                { name: 'Robin Engström', gravatar: 'd3389ec4c8f9a0d7d0500ec982a35099'}
+                { name: 'Michael Håkansson', gravatar: 'e12d2965870d5054f901b088ab692d3d', twitter: 'michaelhak' },
+                { name: 'Robin Engström', gravatar: 'd3389ec4c8f9a0d7d0500ec982a35099' }
             ]
         }
     });
