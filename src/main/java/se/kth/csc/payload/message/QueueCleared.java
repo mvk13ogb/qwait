773b4b84c0717579a64629ab1eb21ca4d87498d4
Casper Winsnes <cwinsnes92@gmail.com>
2014-05-12 15:06:32 +0200

    remove unused imports and fields

--- src/main/java/se/kth/csc/payload/message/QueueCleared.java
+++ src/main/java/se/kth/csc/payload/message/QueueCleared.java
@@ -1,14 +1,10 @@
 package se.kth.csc.payload.message;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import se.kth.csc.model.QueuePosition;
-
-import java.util.Set;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME)
 public class QueueCleared {
     private final String name;
-    private Set<QueuePosition> positions;
 
     public QueueCleared(String name) {
         this.name = name;
