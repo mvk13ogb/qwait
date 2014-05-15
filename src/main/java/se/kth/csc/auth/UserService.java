575f70607b2540c9f2e2ce0478cfcf797cc4a723
Adrian Blanco <adrianblp@gmail.com>
2014-05-15 21:10:21 +0200

    remove mistake, lol

--- src/main/java/se/kth/csc/auth/UserService.java
+++ src/main/java/se/kth/csc/auth/UserService.java
@@ -44,7 +44,6 @@ public class UserService implements AuthenticationUserDetailsService<Authenticat
         if (account == null) {
             account = new Account();
             account.setPrincipalName(token.getName());
-            account.setAdmin(true);
             for (GrantedAuthority grantedAuthority : token.getAuthorities()) {
                 if (Role.ADMIN.getAuthority().equals(grantedAuthority.getAuthority())) {
                     account.setAdmin(true);
