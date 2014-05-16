57c2a83e113790769efb6dc5d2e4e61d15c2f91c
David Flemström <david.flemstrom@gmail.com>
2014-05-16 15:35:45 +0200

    Add license blocks to all .java files

--- src/main/java/se/kth/csc/controller/ApiProviderImpl.java
+++ src/main/java/se/kth/csc/controller/ApiProviderImpl.java
@@ -1,5 +1,26 @@
 package se.kth.csc.controller;
 
+/*
+ * #%L
+ * QWait
+ * %%
+ * Copyright (C) 2013 - 2014 KTH School of Computer Science and Communication
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
 import com.google.common.collect.ImmutableSet;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
